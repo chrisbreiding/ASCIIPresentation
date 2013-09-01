9944f63bb34fd860f2f8d236e2591a39c263202e
Chris Breiding <chrisbreiding@gmail.com>
2013-09-01 17:47:39 -0400

    add boilerplate for package functionality

---
+++ ASCIIPresentation.py
@@ -0,0 +1,22 @@
+import sublime_plugin
+from pyfiglet import Figlet
+
+
+class ConvertTitleCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        pass
+
+
+class ConvertHeadingCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        pass
+
+
+class AddTerminalCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        pass
+
+
+class NewSlideCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        pass
