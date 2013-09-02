f471d7ff9add6cc69f30da3bdeaed7f83be20880
Chris Breiding <chrisbreiding@gmail.com>
2013-09-02 15:48:26 -0400

    fill in add terminal command

--- ASCIIPresentation.py
+++ ASCIIPresentation.py
@@ -17,30 +17,26 @@ def separate_indentation_from_text(text):
 def heading_text(font, text):
     indentation, text = separate_indentation_from_text(text)
     ascii_text = indent_text(indentation, Figlet(font=font).renderText(text))
-
-    return """##
-%s
-##
-""" % ascii_text
+    return "##\n%s\n##\n" % ascii_text
 
 def region_extended_back(region):
     return sublime.Region(region.begin() - 1, region.end())
 
-def region_extended_back_has_line_break(view, region):
+def at_beginning_of_line(view, region):
     if region.begin() <= 0:
         return True
     return re.search("\n", view.substr(region_extended_back(region)))
 
 def text_extended_to_line_beginning(view, region):
     text = view.substr(region)
-    no_line_break = not region_extended_back_has_line_break(view, region)
+    no_line_break = not at_beginning_of_line(view, region)
 
     while no_line_break:
         region = region_extended_back(region)
         view.sel().subtract(region)
         view.sel().add(region)
         text = view.substr(region)
-        no_line_break = not region_extended_back_has_line_break(view, region)
+        no_line_break = not at_beginning_of_line(view, region)
 
     return text, region
 
@@ -62,6 +58,13 @@ def convert_title(view, edit, font_setting):
 
     set_cursors_to_ends_of_selections(view)
 
+def words_before_cursor(view, region):
+    text, region = text_extended_to_line_beginning(view, region)
+    return re.search("\S+", text)
+
+def white_space_before_cursor(view, region):
+    return not at_beginning_of_line(view, region) and re.search("\s+", view.substr(region_extended_back(region)))
+
 
 class ConvertTitleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -75,7 +78,47 @@ class ConvertHeadingCommand(sublime_plugin.TextCommand):
 
 class AddTerminalCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        pass
+        for region in reversed(self.view.sel()):
+            terminal = self.terminal()
+            replacement = ""
+
+            if region.empty():
+                text = self.view.substr(region)
+                if words_before_cursor(self.view, region):
+                    # append
+                    replacement = text + "\n" + terminal
+                elif at_beginning_of_line(self.view, region):
+                    # prepend
+                    replacement = terminal + text
+                else:
+                    # prepend and indent
+                    indentation, region = text_extended_to_line_beginning(self.view, region)
+                    terminal = indent_text(indentation, terminal)
+                    replacement = terminal + text
+            else:
+                if white_space_before_cursor(self.view, region):
+                    # replace and indent
+                    text, region = text_extended_to_line_beginning(self.view, region)
+                    indentation, text = separate_indentation_from_text(text)
+                    replacement = indent_text(indentation, terminal)
+                else:
+                    # replace
+                    replacement = terminal
+
+            self.view.replace(edit, region, replacement)
+
+        set_cursors_to_ends_of_selections(self.view)
+
+    def terminal(self):
+        settings = sublime.load_settings('ASCIIPresentation.sublime-settings')
+        width = settings.get('terminal_width')
+        height = settings.get('terminal_height')
+
+        hyphens = '-' * width
+        spaces = ' ' * width
+        title_bar_spaces = ' ' * (width - 3)
+        lines = "|%s|\n" % spaces
+        return " %s\n|ooo%s|\n|%s|\n%s %s\n" % (hyphens, title_bar_spaces, hyphens, lines * height, hyphens)
 
 
 class NewSlideCommand(sublime_plugin.TextCommand):
